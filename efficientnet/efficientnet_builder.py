6d02646d48b3c4bcd2354cf8e53eb15e6c4057de
matthewygf <yyygggfff@hotmail.com>
2019-06-30 19:23:41 +0100

    WIP - first pipeline training ready

--- efficientnet/efficientnet_builder.py
+++ efficientnet/efficientnet_builder.py
@@ -116,7 +116,8 @@ class BlockDecoder(object):
 def efficientnet(width_coefficient=None,
                  depth_coefficient=None,
                  dropout_rate=0.2,
-                 drop_connect_rate=0.2):
+                 drop_connect_rate=0.2,
+                 data_format='channels_first'):
   """Creates a efficientnet model."""
   blocks_args = [
       'r1_k3_s11_e1_i32_o16_se0.25', 'r2_k3_s22_e6_i16_o24_se0.25',
@@ -129,7 +130,7 @@ def efficientnet(width_coefficient=None,
       batch_norm_epsilon=1e-3,
       dropout_rate=dropout_rate,
       drop_connect_rate=drop_connect_rate,
-      data_format='channels_last',
+      data_format=data_format,
       num_classes=1000,
       width_coefficient=width_coefficient,
       depth_coefficient=depth_coefficient,
@@ -139,13 +140,13 @@ def efficientnet(width_coefficient=None,
   return decoder.decode(blocks_args), global_params
 
 
-def get_model_params(model_name, override_params):
+def get_model_params(model_name, override_params, data_format='channels_first'):
   """Get the block args and global params for a given model."""
   if model_name.startswith('efficientnet'):
     width_coefficient, depth_coefficient, _, dropout_rate = (
         efficientnet_params(model_name))
     blocks_args, global_params = efficientnet(
-        width_coefficient, depth_coefficient, dropout_rate)
+        width_coefficient, depth_coefficient, dropout_rate, data_format=data_format)
   else:
     raise NotImplementedError('model name is not pre-defined: %s' % model_name)
 
@@ -154,8 +155,8 @@ def get_model_params(model_name, override_params):
     # in global_params.
     global_params = global_params._replace(**override_params)
 
-  tf.logging.info('global_params= %s', global_params)
-  tf.logging.info('blocks_args= %s', blocks_args)
+  tf.compat.v1.logging.info('global_params= %s', global_params)
+  tf.compat.v1.logging.info('blocks_args= %s', blocks_args)
   return blocks_args, global_params
 
 
@@ -163,7 +164,8 @@ def build_model(images,
                 model_name,
                 training,
                 override_params=None,
-                model_dir=None):
+                model_dir=None,
+                data_format='channels_first'):
   """A helper functiion to creates a model and returns predicted logits.
 
   Args:
@@ -183,7 +185,7 @@ def build_model(images,
     When override_params has invalid fields, raises ValueError.
   """
   assert isinstance(images, tf.Tensor)
-  blocks_args, global_params = get_model_params(model_name, override_params)
+  blocks_args, global_params = get_model_params(model_name, override_params, data_format=data_format)
 
   if model_dir:
     param_file = os.path.join(model_dir, 'model_params.txt')
@@ -191,7 +193,7 @@ def build_model(images,
       if not tf.gfile.Exists(model_dir):
         tf.gfile.MakeDirs(model_dir)
       with tf.gfile.GFile(param_file, 'w') as f:
-        tf.logging.info('writing to %s' % param_file)
+        tf.compat.v1.logging.info('writing to %s' % param_file)
         f.write('model_name= %s\n\n' % model_name)
         f.write('global_params= %s\n\n' % str(global_params))
         f.write('blocks_args= %s\n\n' % str(blocks_args))
@@ -204,7 +206,7 @@ def build_model(images,
   return logits, model.endpoints
 
 
-def build_model_base(images, model_name, training, override_params=None):
+def build_model_base(images, model_name, training, override_params=None, data_format='channels_first'):
   """A helper functiion to create a base model and return global_pool.
 
   Args:
@@ -223,11 +225,11 @@ def build_model_base(images, model_name, training, override_params=None):
     When override_params has invalid fields, raises ValueError.
   """
   assert isinstance(images, tf.Tensor)
-  blocks_args, global_params = get_model_params(model_name, override_params)
+  blocks_args, global_params = get_model_params(model_name, override_params, data_format=data_format)
 
   with tf.variable_scope(model_name):
     model = efficientnet_model.Model(blocks_args, global_params)
     features = model(images, training=training, features_only=True)
 
   features = tf.identity(features, 'global_pool')
-  return features, model.endpoints
+  return features, model.endpoints, model
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICE'] = '3'
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
 from efficientnet import efficientnet_builder
-import os
 import matplotlib.pyplot as plt
+from utils_ops import Conv2D_Pad
 from PIL import Image
-tf.enable_eager_execution()
+import time
 
 MEAN_RGB = [0.485, 0.456, 0.406]
 STDDEV_RGB = [0.229, 0.224, 0.225]
@@ -17,48 +19,95 @@ class DataLoader(object):
     self.path = path = os.path.join(os.getcwd(), "train_dir")
     df = pd.read_csv('train.csv')
     # id_code,diagnosis
-    self.names = np.asarray(df.id_code,dtype=str)
+    self.names = np.asarray(df.id_code, dtype=str)
+    def pad_dir_name(x):
+      filename = x+".png"
+      filename = os.path.join(self.path, filename)
+      return filename
+    self.names = np.array([pad_dir_name(name) for name in self.names])
     self.labels = np.asarray(df.diagnosis, dtype=int)
     self.image_size = image_size
     # manual train test split
     train_size = int(math.ceil(self.names.shape[0] * 0.8))
-    x_train = self.names[:train_size]
-    y_train = self.labels[:train_size]
-    x_test = self.names[train_size:]
-    y_test = self.labels[train_size:]
-    self.train_dataset = tf.compat.v1.data.Dataset.from_tensor_slices((x_train, y_train))
-    self.test_dataset = tf.compat.v1.data.Dataset.from_tensor_slices((x_test, y_test))
-  
-  def _parse_func(self, is_training=True):
-    filename = names+".png"
-    filename = os.path.join(self.path, filename)
-    read = tf.io.read_file(filename)
-    decode = tf.image.decode_image(read)
-    image = tf.cast(decode, tf.float32) / 255
-    # TODO: RANDOM CROP, MEAN
-    
+    self.x_train = self.names[:train_size]
+    self.y_train = self.labels[:train_size]
+    self.x_val = self.names[train_size:]
+    self.y_val = self.labels[train_size:]
+    assert self.x_train.shape[0] == self.y_train.shape[0]
+    assert self.x_val.shape[0] == self.y_val.shape[0]
+    self.train_dataset = tf.compat.v1.data.Dataset.from_tensor_slices((self.x_train, self.y_train))
+    self.val_dataset = tf.compat.v1.data.Dataset.from_tensor_slices((self.x_val, self.y_val))
+    self.is_cuda = tf.test.is_built_with_cuda() and tf.test.is_gpu_available()
+    self.ckpt_dir = os.path.join(os.getcwd(), "ckpt_dir")
+    self.model_dir = os.path.join(os.getcwd(), "model_dir")
+    self.batch_size=4
+    self.num_batch_per_epoch = (train_size + self.batch_size - 1) // self.batch_size
 
+  def build_datasets(self):
+    self._build_train_dataset()
+    self._build_val_dataset()
+
+  def _build_train_dataset(self):
+    self.train_dataset = self.train_dataset.map(
+      self._parse_func_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    self.train_dataset = self.train_dataset.shuffle(buffer_size=8)
+    self.train_dataset = self.train_dataset.repeat()
+    self.train_dataset = self.train_dataset.batch(self.batch_size)
+    self.train_dataset = self.train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    self.train_iterator = tf.compat.v1.data.make_one_shot_iterator(self.train_dataset)
+
+  def _build_val_dataset(self):
+    self.val_dataset = self.val_dataset.map(
+      self._parse_func_val, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    self.val_dataset = self.val_dataset.shuffle(buffer_size=8)
+    self.val_dataset = self.val_dataset.repeat()
+    self.val_dataset = self.val_dataset.batch(self.batch_size)
+    self.val_iterator = tf.compat.v1.data.make_one_shot_iterator(self.val_dataset)
+
+  def _transform(self, name, is_training=False):
+    read = tf.io.read_file(name)
+    img = tf.image.decode_png(read, channels=3)
+    img = tf.cast(img, tf.float32)
+    img /= 255.0
+    img -= tf.constant(MEAN_RGB, shape=[1, 1, 3], dtype=img.dtype)
+    img /= tf.constant(STDDEV_RGB, shape=[1, 1, 3], dtype=img.dtype)
+    img = tf.compat.v1.image.resize_image_with_pad(
+      img, self.image_size, self.image_size,
+      method=tf.image.ResizeMethod.BICUBIC)
+    if is_training:
+      log_probs = tf.math.log([[10., 10., 10., 10.]])
+      k = tf.random.categorical(log_probs, 1, dtype=tf.int32)
+      img = tf.image.random_flip_left_right(img)
+      img = tf.image.random_flip_up_down(img)
+      img = tf.image.rot90(img, k[0][0])
+      img = tf.image.random_brightness(img, 0.1)
+      img = tf.image.random_saturation(img, lower=0.8, upper=1.2)
+      img = tf.image.random_hue(img, max_delta=0.1)
+      img = tf.image.random_contrast(img, 0.7, 1.3)
+    return img
+
+  def _parse_func_train(self, names, labels):
+    img = self._transform(names, is_training=True)
+    if self.is_cuda:
+      img = tf.transpose(img, [2,0,1])
+      img = tf.reshape(img, [3, self.image_size, self.image_size], name='reshape_img_first')
+    else:
+      img = tf.reshape(img, [self.image_size, self.image_size, 3], name='reshape_img_last')
+    return (img, labels)
+  
+  def _parse_func_val(self, names, labels):
+    img = self._transform(names)
+    if self.is_cuda:
+      img = tf.transpose(img, [2,0,1])
+      img = tf.reshape(img, [3, self.image_size, self.image_size])
+    else:
+      img = tf.reshape(img, [self.image_size, self.image_size, 3])
+    return (img, labels)
+    
   def get_test_image(self, i):
     test_file = self.get_image_path(i)
     if tf.io.gfile.exists(test_file):
-      read = tf.io.read_file(test_file)
-      img = tf.image.decode_image(read, channels=3)
-      img = tf.cast(img, tf.float32)
-      img /= 255.0
-      img -= tf.constant(MEAN_RGB, shape=[1, 1, 3], dtype=img.dtype)
-      img /= tf.constant(STDDEV_RGB, shape=[1, 1, 3], dtype=img.dtype)
-      img = tf.compat.v1.image.resize_images(
-        img, [self.image_size, self.image_size], 
-        method=tf.image.ResizeMethod.BICUBIC,
-        preserve_aspect_ratio=True)
-      
-      log_probs = tf.math.log([[10., 10., 10., 10.]])
-      k = tf.random.categorical(log_probs, 1, dtype=tf.int32)
-      img = tf.image.rot90(img, k[0][0])
-      img = tf.image.random_flip_left_right(img)
-      img = tf.image.random_flip_up_down(img)
-      img = tf.image.random_brightness(img, 0.1)
-      img = tf.image.random_contrast(img, 0.8, 1.2)
+      img = self._transform(test_file)
       return img
 
   def get_image_path(self,i):
@@ -74,13 +123,129 @@ class DataLoader(object):
       img = Image.fromarray(np.uint8(img.numpy()))
       img.save("test%d.png" % index)
 
+def restore_model(sess, ckpt_dir, enable_ema=True, export_ckpt=None):
+  sess.run(tf.compat.v1.global_variables_initializer())
+  ckpt_dir = os.path.join(ckpt_dir, 'efficient')
+  ckpt = tf.compat.v1.train.latest_checkpoint(ckpt_dir)
+  if enable_ema:
+    ema = tf.compat.v1.train.ExponentialMovingAverage(decay=0.0)
+    ema_vars = (tf.compat.v1.trainable_variables() +
+     tf.compat.v1.moving_average_variables())
+    for v in tf.compat.v1.global_variables():
+      if ('moving_mean' in v.name or 
+          'moving_variance' in v.name):
+        ema_vars.append(v)
+    ema_vars = list(set(ema_vars))
+    var_dict = ema.variables_to_restore(ema_vars)
+    ema_assign_op = ema.apply(ema_vars)
+  else:
+    var_dict = None
+    ema_assign_op = None
+
+  sess.run(tf.compat.v1.global_variables_initializer())
+  saver = tf.compat.v1.train.Saver(var_dict, max_to_keep=1)
+  saver.restore(sess, ckpt)
+
+  if export_ckpt:
+    if ema_assign_op is not None:
+      sess.run(ema_assign_op)
+    saver = tf.compat.v1.train.Saver(max_to_keep=1)
+    saver.save(sess, export_ckpt)
+
+class FinalClassify(tf.keras.layers.Layer):
+  def __init__(self, bn_axis, data_format, trainable=True, name=None, dtype=None, dynamic=False, **kwargs):
+    super(FinalClassify, self).__init__(trainable=trainable, name=name, dtype=dtype, dynamic=dynamic, **kwargs)
+    self.conv1 = Conv2D_Pad(256, 3, 1, data_format=data_format)
+    self.bn1 = tf.keras.layers.BatchNormalization(axis=bn_axis)
+    self.relu = tf.keras.layers.ReLU()
+    self.conv2 = Conv2D_Pad(128, 1, 1, data_format=data_format)
+    self.bn2 = tf.keras.layers.BatchNormalization(axis=bn_axis)
+    self.avg = tf.keras.layers.GlobalAveragePooling2D(data_format=data_format)
+    self.dropout = tf.keras.layers.Dropout(0.25)
+    self.linear = tf.keras.layers.Dense(5, activation=None)
+
+  def call(self, inputs, training=True):
+    x = self.conv1(inputs)
+    x = self.bn1(x)
+    x = self.relu(x)
+    x = self.conv2(x)
+    x = self.bn2(x)
+    x = self.relu(x)
+    x = self.avg(x)
+    x = self.dropout(x)
+    x = self.linear(x)
+    return x
+
 def main():
   model_name = 'efficientnet-b5' 
   _, _, image_size, _ = efficientnet_builder.efficientnet_params(model_name)
-  # efficientnet-b5 should be 465x465
-  loader = DataLoader(image_size)
-  loader.save_sample_images(10)
-  # TODO: build model, fine tune
+
+  g = tf.Graph()
+  with tf.compat.v1.Session(graph=g) as sess:
+    loader = DataLoader(image_size)
+    # loader.save_sample_images(10)
+    loader.build_datasets()
+    data_format = 'channels_first' if loader.is_cuda else 'channels_last'
+    bn_axis = 1 if loader.is_cuda else -1
+    batch_x, batch_y = loader.train_iterator.get_next()
+    logits, _, model = efficientnet_builder.build_model_base(
+      batch_x, model_name, training=False, data_format=data_format)
+    
+    restore_model(sess, loader.ckpt_dir)
+    
+    # NOTE: transfer learning
+    model.trainable = False
+
+    global_step = tf.Variable(
+      0, dtype=tf.int32, trainable=False, name="global_step")
+
+    with tf.compat.v1.variable_scope("final"):
+      final_classifier = FinalClassify(bn_axis, data_format)
+      logits = final_classifier(logits)
+
+    log_probs = tf.compat.v1.nn.sparse_softmax_cross_entropy_with_logits(
+      logits=logits, labels=batch_y
+    )
+
+    loss = tf.reduce_mean(log_probs)
+    preds = tf.argmax(logits, axis=1)
+    
+    train_acc = tf.equal(preds, batch_y)
+    train_acc = tf.cast(train_acc, tf.int32)
+    train_acc = tf.reduce_sum(train_acc)
+
+    # TODO: LR schedule rate.
+    # lr = tf.compat.v1.train.exponential_decay(
+    #   0.03, tf.maximum((global_step - 50), 0),
+    #   100, 0.05, staircase=True
+    # )
+    # lr = tf.maximum(lr, 0.0001)
+    final_var = [var for var in tf.compat.v1.trainable_variables() if var.name.startswith('final')]
+    saver = tf.compat.v1.train.Saver(final_var, max_to_keep=1)
+
+    train_op = (tf.compat.v1.train.GradientDescentOptimizer(0.001, use_locking=True)
+            .minimize(loss, 
+              global_step=global_step, 
+              var_list=final_var))
+
+    sess.run(tf.compat.v1.global_variables_initializer())
+
+    # train phrase
+    start_time = time.time()
+    for i in range(loader.num_batch_per_epoch * 10):
+      _, acc, loss_val = sess.run([train_op, train_acc, loss])
+      if i % 50 == 0:
+        ran = float(start_time - time.time() )/ 60.0
+        tf.compat.v1.logging.info("Min: %.2f, Step: %d, train_acc: %d, loss_val: %d" % (ran, i, acc, loss_val))
+      
+      if i % loader.num_batch_per_epoch == 0:
+        final_dir = os.path.join(loader.ckpt_dir, 'final')
+        if not os.path.exists(final_dir):
+          os.makedirs(final_dir)
+        saver.save(sess, final_dir, global_step=global_step)
+
+    # TODO: set to testphrase keras.
+
 
 if __name__=="__main__":
   main()
